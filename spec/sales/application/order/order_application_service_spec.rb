97319eb1781f23fd5d3a5937166a43304ab8bbbb
Michal Lomnicki <michal.lomnicki@gmail.com>
2016-02-17 23:53:46 +0100

    Refactor OrderApplicationService specs

--- spec/sales/application/order/order_application_service_spec.rb
+++ spec/sales/application/order/order_application_service_spec.rb
@@ -6,62 +6,52 @@ RSpec.describe Sales::Application::Order::OrderApplicationService do
   let(:event_store) { FakeEventStore.new }
   let(:repository)  { Sales::Adapter::Persistence::OrderRepository.new(event_store) }
 
+  let(:order_id)    { 1 }
+  let(:customer_id) { 15 }
+  let(:product_id)  { 30 }
+
   let(:create_order_command) do
-    Sales::Application::Order::CreateOrderCommand.new(
-      order_id:    1,
-      customer_id: 15
-    )
+    Sales::Application::Order::CreateOrderCommand.new(order_id: order_id, customer_id: customer_id)
   end
 
   let(:add_item_command) do
-    Sales::Application::Order::AddItemToOrderCommand.new(
-      order_id:   1,
-      product_id: 2
-    )
+    Sales::Application::Order::AddItemToOrderCommand.new(order_id: order_id, product_id: product_id)
   end
 
   let(:expired_order_command) do
-    Sales::Application::Order::ExpireOrderCommand.new(
-      order_id: 1
-    )
+    Sales::Application::Order::ExpireOrderCommand.new(order_id: order_id)
   end
 
   let(:service) { described_class.new(repository) }
 
   describe "#create_order" do
-    let(:command) { create_order_command }
-
     it "creates a new order" do
       service.add_item_to_order(add_item_command)
-      service.create_order(command)
+      service.create_order(create_order_command)
 
       expect(event_store).to receive_events([
-        Sales::Domain::Order::ItemAddedToOrder.new(order_id: command.order_id, product_id: add_item_command.product_id),
-        Sales::Domain::Order::OrderCreated.new(order_id: command.order_id, customer_id: command.customer_id)
+        Sales::Domain::Order::ItemAddedToOrder.new(order_id: order_id, product_id: product_id),
+        Sales::Domain::Order::OrderCreated.new(order_id: order_id, customer_id: customer_id)
       ])
     end
   end
 
   describe "#add_item_to_order" do
-    let(:command) { add_item_command }
-
     it "adds an item to order" do
-      service.add_item_to_order(command)
+      service.add_item_to_order(add_item_command)
 
       expect(event_store).to receive_events([
-        Sales::Domain::Order::ItemAddedToOrder.new(order_id: command.order_id, product_id: command.product_id)
+        Sales::Domain::Order::ItemAddedToOrder.new(order_id: order_id, product_id: product_id)
       ])
     end
   end
 
   describe "#expire_order" do
-    let(:command) { expired_order_command }
-
     it "expires an order" do
-      service.expire_order(command)
+      service.expire_order(expired_order_command)
 
       expect(event_store).to receive_events([
-        Sales::Domain::Order::OrderExpired.new(order_id: command.order_id)
+        Sales::Domain::Order::OrderExpired.new(order_id: order_id)
       ])
     end
   end
