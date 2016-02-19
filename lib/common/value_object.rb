51c741077417bd146a88e9800d3ece1ff57f5d48
Michal Lomnicki <michal.lomnicki@gmail.com>
2016-02-20 00:20:32 +0100

    Make ValueObject immutable

--- lib/common/value_object.rb
+++ lib/common/value_object.rb
@@ -1,6 +1,10 @@
 class ValueObject < Struct
   def self.new(*attributes, &block)
     super.tap do |struct_class|
+      struct_class.class_eval do
+        private *attributes.map { |attr| "#{attr}=" }
+      end
+
       struct_class.class_eval <<-RUBY
         def initialize(#{attributes.map { |attr| "#{attr}:" }.join(', ')})
           super(#{attributes.join(', ')})
