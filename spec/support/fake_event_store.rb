25f04440acb352c96eedefaf9546a141abb6d2b7
Michal Lomnicki <michal.lomnicki@gmail.com>
2016-02-27 00:00:55 +0100

    Add simple pub/sub functionality to FakeEventStore

--- spec/support/fake_event_store.rb
+++ spec/support/fake_event_store.rb
@@ -2,16 +2,24 @@ class FakeEventStore
   attr_reader :events, :published
 
   def initialize
-    @published = []
-    @streams = Hash.new { |hash, stream| hash[stream] = [] }
+    @published   = []
+    @streams     = Hash.new { |hash, stream| hash[stream] = [] }
+    @subscribers = Hash.new { |hash, event_type| hash[event_type] = [] }
   end
 
   def publish_event(event, stream)
     published << event
     @streams[stream] << event
+    @subscribers[event.class].each { |subscriber| subscriber.handle(event) }
   end
 
   def read_all_events(stream)
     @streams[stream]
+  end
+
+  def subscribe(subscriber, event_types)
+    event_types.each do |event_type|
+      @subscribers[event_type] << subscriber
+    end
   end
 end
